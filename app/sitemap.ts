85e0eabdbdc1cdc0b6ac9cd9fc70129872aeb37f
U CAN AGÊNCIA DE MARKETING <ageucan@gmail.com>
2026-05-18 14:02:26 -0300

    Make sitemap route static for export build

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,10 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://www.mrmegamarca.com.br";
-  const lastModified = new Date();
+  const lastModified = new Date("2026-05-18");
 
   return [
     {
